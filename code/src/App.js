2383f09ae7ca9425ef140cf4f0e350b2a24e1d55
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-03-19 15:36:24 +0100

    structured in app

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import { HappyThought } from "./components/HappyThought"
+import { HappyForm } from "./components/HappyForm"
+
+const url = "https://technigo-thoughts.herokuapp.com/"
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [postedMessage, setPostedMessage] = useState("")
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => setThoughts(json))
+  }, [])
+
   return (
     <div>
       Find me in src/app.js jag då?!
