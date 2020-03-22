28be738670c27882f4463d9be4467e40a98d8d96
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-03-22 16:33:16 +0100

    like uppdates

--- code/src/components/HappyHeart.js
+++ code/src/components/HappyHeart.js
@@ -1,22 +1,38 @@
 import React, { useState } from "react"
+import moment from 'moment'
 
-
-export const HappyHeart = ({ id }) => {
+export const HappyHeart = ({ id, hearts, date}) => {
+  const [likes, setLikes] = useState(hearts)
 
   const handleClick = () => {
     fetch(`https://technigo-thoughts.herokuapp.com/${id}/like`, {
         method: "POST",
         body: "",
         headers: { "Content-Type": "application/json" }
-    })
-}  
+      }).then(() => {
+        setLikes(likes + 1)
+      })
+    }
 
 return (
 
-    <button className="heart-button"
+  <div className="content-container">
+
+  <section className="icon-container">
+
+  <button className="heart-button"
     onClick={handleClick} >
           ❤️ 
 		</button>
+
+
+      <div className="heart-like"> x {likes}</div>
+
+  </section>
+
+    <div className="timestamp">{moment(date).fromNow()}</div>
+    
+  </div>  
   
 );
 }  
--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -3,28 +3,20 @@ import moment from 'moment'
 import "./HappyThought.css"
 import { HappyHeart } from "./HappyHeart"
 
-export const HappyThought = ({message, hearts, date, id }) => {
+export const HappyThought = ({message, id, date, hearts }) => {
 
 	return (
 		<section className="thought-container">
 			<p>{message}</p>
 
-      <div className="content-container">
 
-			  <section className="icon-container">
-          		
-
-        <HappyHeart
+      <HappyHeart
               id={id}
+              date={date}
+              hearts={hearts}
           />
 
-          <div className="heart-like"> x {hearts}</div>
-
-          </section>
-
-          <div className="timestamp">{moment(date).fromNow()}</div>
-				  
-        </div>  	
+    
 		</section>
 	);
 };
